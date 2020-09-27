23debbe38ed399a95f8c4717c3dcba342c8fe0ae
abrahamBerzunza <abraham.berzunza@gmail.com>
2020-09-27 16:09:38 -0500

    exercise 3 added

---
+++ exercises/exercise-3.js
@@ -0,0 +1,16 @@
+/*
+  Write a function that gets any object
+  and returns the sum of the lenghts of
+  all the strings inside the object
+
+
+  Considerations: Values can be nested and datypes Number, String or Object.
+*/
+
+function deepStringSum() {
+  // TODO: implement
+}
+
+module.exports = {
+  deepStringSum
+};
