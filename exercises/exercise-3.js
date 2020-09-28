240c1474420988e2325846dd18023139ea74a720
Arturo Cantú <a01196412@itesm.mx>
2020-09-28 16:14:32 -0500

    Ex 3 Completed

--- exercises/exercise-3.js
+++ exercises/exercise-3.js
@@ -7,9 +7,35 @@
   Considerations: Values can be nested and datypes Number, String or Object.
 */
 
-function deepStringSum() {
-  // TODO: implement
+function deepStringSum(obj) {
+	let sum = 0;
+	for (key in obj) {
+		if(typeof obj[key] === 'object'){
+			sum += deepStringSum(obj[key]);
+		} else if (typeof obj[key] === 'string'){
+			sum += obj[key].length;
+		} 
+	}
+	return sum;
 }
+
+/* Traté de hacerlo con pura recursividad pero se me cicla
+function deepStringSum(obj, sum = 0) {
+	while(Object.entries(obj).length !== 0){
+		for (key in obj) {
+			if(typeof obj[key] === 'object'){
+				deepStringSum(obj[key], sum);
+			} else if (typeof obj[key] === 'string'){
+				sum += obj[key].length;
+				console.log(sum);
+				delete obj[key];
+				deepStringSum(obj, sum);
+			}
+		}
+	}
+	return 30;
+}
+*/
 
 module.exports = {
   deepStringSum
