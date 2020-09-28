ec50fb17fdb65c4a39b1f4f8684a72450ae81c32
alexG <alejandro.gonzalez.barriga@gmail.com>
2020-09-28 13:16:33 -0500

    recursion exersise passed

--- exercises/exercise-3.js
+++ exercises/exercise-3.js
@@ -7,8 +7,13 @@
   Considerations: Values can be nested and datypes Number, String or Object.
 */
 
-function deepStringSum() {
-  // TODO: implement
+function deepStringSum(obj) {
+  let counter = 0;
+  for (var i in obj) {
+    if (typeof obj[i] == 'object') counter += deepStringSum(obj[i]);
+    if (typeof obj[i] == 'string') counter += obj[i].length
+  }
+  return counter;
 }
 
 module.exports = {
